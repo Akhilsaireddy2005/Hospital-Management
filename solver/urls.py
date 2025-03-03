9cd8f6a15faa100c383fb5afa50c6b8d2a844a5e
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2025-03-03 14:18:07 +0530

    15

--- solver/urls.py
+++ solver/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views  
 
-app_name = 'solver'
-
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path('', views.home, name='solver'), 
     path('mpweb/', views.mpweb, name='mpweb'),
-    path('solve-lp/', views.solve_lp, name='solve_lp'),
+    path('solve-lp/', views.solve_lp, name='graphical_lp.html'),
     path('lpsimplex/', views.lpsimplex, name='lpsimplex'),
     path('solve_simplex/', views.solve_simplex, name='solve_simplex'),
     path('transportation/', views.transport, name='transportation'),  
