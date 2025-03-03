2db947894d8a3f9e45ed1675ad45ab835e9abfdb
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2025-03-03 13:26:37 +0530

    13

--- hospital_management/urls.py
+++ hospital_management/urls.py
@@ -16,17 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from django.views.decorators.http import require_http_methods
-
-# Custom logout view that accepts both GET and POST
-class CustomLogoutView(auth_views.LogoutView):
-    http_method_names = ['get', 'post']
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hospital.urls')),
     path('solver/', include('solver.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='hospital/login.html'), name='login'),
-    path('logout/', CustomLogoutView.as_view(next_page='login'), name='logout'),
 ]
