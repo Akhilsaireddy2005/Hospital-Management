a1f2dd2284dd124b9114458f5840b9995aa6da79
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2025-04-10 15:05:06 +0530

    94

--- hospital_management/urls.py
+++ hospital_management/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/dashboard/', permanent=True)),  # Redirect root to dashboard
     path('', include('hospital.urls')),
     path('solver/', include('solver.urls')),
 ]
